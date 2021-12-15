90cb5d90e902736434d7b132ce39816f9377f64b
alireza <mjavaheri.alireza@gmail.com>
2021-12-15 09:11:08 +0330

    creat url in blog

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView
+from .models import Article
 # Create your views here.
+class Articlelist(ListView):
+    def get_queryset(self):
+        return Article.objects.all
